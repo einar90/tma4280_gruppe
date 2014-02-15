5c0ac7a9b2e0971c579e3bac9814e2d20561e03b
einar90 <einar.baumann@gmail.com>
2014-02-15 19:27:40 +0100

    Added stub

---
+++ code/ex4.c
@@ -0,0 +1,12 @@
+#include <mpi.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "common.h"
+
+int main(int argc, char const *argv[])
+{
+  printf("Ran.\n");
+  return 0;
+}
