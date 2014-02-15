e17edf7ed78adc7ed35c0fc046ace4819e049a8f
Dzenan <dumpor@gmail.com>
2014-02-15 20:19:55 +0100

    Fjernet populateVector, common hadde dette også..

--- code/ex4.c
+++ code/ex4.c
@@ -6,14 +6,6 @@
 
 #include "common.h"
 
-void populateVector(float* array, int n) {
-	for(int i = 0; i < n-1; i++) {
-		array[i] = 1.0 / (i*i);
-	}
-}
-
-
-
 /*
    Function for testing the OpenMP lib
  */
