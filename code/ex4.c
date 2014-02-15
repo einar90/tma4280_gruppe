69d59c46aaa06aacc7abf651128da845a5a021a3
einar90 <einar.baumann@gmail.com>
2014-02-15 20:08:09 +0100

    Created MPI test

--- code/ex4.c
+++ code/ex4.c
@@ -6,6 +6,9 @@
 #include "common.h"
 
 
+/*
+   Function for testing the OpenMP lib
+ */
 void openMpTest() {
   #pragma omp parallel for schedule(static)
   for (int i = 0; i < 10; ++i)
@@ -15,8 +18,36 @@ void openMpTest() {
 }
 
 
-int main(int argc, char const *argv[])
+void mpiTest(int rank) {
+
+}
+
+
+int main(int argc, char** argv)
 {
   openMpTest();
+
+  // MPI Testing
+  int rank, size, i, tag;
+  MPI_Status status;
+  char message[20];
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  tag = 100;
+
+  if (rank == 0) {
+    strcpy(message, "Hello world!");
+    for (i=1; i < size; ++i)
+      MPI_Send(message, 13, MPI_CHAR, i, tag, MPI_COMM_WORLD);
+  } else
+    MPI_Recv(message, 13, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
+
+  printf("process %d: %s\n", rank, message);
+
+  MPI_Finalize();
+
   return 0;
 }
