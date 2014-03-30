68c663f6ae04a7157fefaaa5ced220027546e5f5
einar90 <einar.baumann@gmail.com>
2014-03-30 19:33:46 +0200

    Started over. Works now.

--- ex6/code/only_omp/poisson/poisson2D.c
+++ ex6/code/only_omp/poisson/poisson2D.c
@@ -3,7 +3,6 @@
 #include <math.h>
 
 #include "common.h"
-#include "solvers.h"
 #include "poissoncommon.h"
 
 double alpha=0.0;
@@ -21,8 +20,6 @@ double source(double x, double y)
 
 
 
-
-
 void DiagonalizationPoisson2Dfst(Matrix b, const Vector lambda)
 {
   int i,j;
@@ -31,13 +28,9 @@ void DiagonalizationPoisson2Dfst(Matrix b, const Vector lambda)
   int N=b->rows+1;
   int NN=4*N;
 
-#pragma omp parallel for schedule(static) private(i)
   for (i=0;i<b->cols;++i)
     fst(b->data[i], &N, buf->data, &NN);
-
   transposeMatrix(ut, b);
-
-#pragma omp parallel for schedule(static) private(i)
   for (i=0;i<ut->cols;++i)
     fstinv(ut->data[i], &N, buf->data, &NN);
 
@@ -45,12 +38,9 @@ void DiagonalizationPoisson2Dfst(Matrix b, const Vector lambda)
     for (i=0;i<b->rows;++i)
       ut->data[j][i] /= (lambda->data[i]+lambda->data[j]+alpha);
 
-#pragma omp parallel for schedule(static) private(i)
   for (i=0;i<b->cols;++i)
     fst(ut->data[i], &N, buf->data, &NN);
   transposeMatrix(b, ut);
-
-#pragma omp parallel for schedule(static) private(i)
   for (i=0;i<ut->cols;++i)
     fstinv(b->data[i], &N, buf->data, &NN);
 
@@ -62,73 +52,59 @@ void DiagonalizationPoisson2Dfst(Matrix b, const Vector lambda)
 
 
 
-
 int main(int argc, char** argv)
 {
   int i, j, N, flag, local;
   Matrix b, e;
   Vector grid, lambda=NULL;
   double time, sum, h;
-
-  flag=12;
-  local = 0;
-
+  flag = 12;
   N=atoi(argv[1]);
-  if (N < 0) {
-    printf("invalid problem size given\n");
-    return 2;
-  }
-  if ((N & (N-1)) != 0) {
+
+  if (flag == 12 && (N & (N-1)) != 0) {
     printf("need N to be a power-of-two for fst-based diagonalization\n");
     return 5;
   }
 
-  // Generate 1D grid from 0 to 1
-  grid = equidistantMesh(0.0, 1.0, N);
+  local = 0;
 
-  // Create matrices
+  grid = equidistantMesh(0.0, 1.0, N);
   b = createMatrix(N-1,N-1);
   e = createMatrix(N-1,N-1);
 
-  // Populate b with the source function evaluated for grid values
   evalMeshInternal2(b, grid, source, local);
-
   h = grid->data[1]-grid->data[0];
   scaleVector(b->as_vec, pow(h, 2));
   evalMeshInternal2(e, grid, exact, local);
   axpy(b->as_vec, e->as_vec, alpha);
 
-  // Generate eigenvalues
+
+
   lambda = generateEigenValuesP1D(N-1);
 
   time = WallTime();
-
-  // Solving system b
   DiagonalizationPoisson2Dfst(b, lambda);
+
 
   printf("elapsed: %f\n", WallTime()-time);
 
-
   axpy(b->as_vec,e->as_vec,-1.0);
-
-  // // Printing b
-  // for (i = 0; i < N; ++i)
-  // {
-  //   for (j = 0; j < N; ++j)
-  //   {
-  //     printf("%4.2f ", b->data[i][j]);
-  //   }
-  //   printf("\n");
-  // }
 
   printf("max error: %e\n", maxNorm(b->as_vec));
 
-
+  for (i = 0; i < b->rows; ++i)
+  {
+    for (j = 0; j < b->cols; ++j)
+    {
+      printf("%f ", b->data[i][j]);
+    }
+    printf("\n");
+  }
 
   freeMatrix(b);
   freeMatrix(e);
   freeVector(grid);
-  if (lambda)
-    freeVector(lambda);
+  freeVector(lambda);
+
   return 0;
 }
